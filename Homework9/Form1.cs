51c668acc2e1b4fcbda626e243fa944fd9426ef1
shuang wu <sweet1w4@163.com>
2021-05-08 01:11:04 +0800

    hw9 remained to be improved

---
+++ Homework9/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+
+namespace Homework9
+{
+    public partial class Form1 : Form
+    {
+        Crawler crawler = new Crawler();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            tb_startURL.DataBindings.Add("Text", crawler, "StartURL");
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btn_crawl_Click(object sender, EventArgs e)
+        {
+            bindingSource1.Clear();
+
+            Match match = Regex.Match(crawler.StartURL, Crawler.urlParseRegex);
+            if (match.Length == 0) return;
+            string host = match.Groups["host"].Value;
+            crawler.HostFilter = "^" + host + "$";
+            crawler.FileFilter = "((.html?|.aspx|.jsp|.php)$|^[^.]+$)";
+            //crawler.Start();
+            new Thread(crawler.Crawl).Start();
+
+        }
+
+        //自动编号
+        private void dgv_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            Rectangle rectangle = new Rectangle(e.RowBounds.Location.X,
+               e.RowBounds.Location.Y,
+               dgv.RowHeadersWidth - 4,
+               e.RowBounds.Height);
+            TextRenderer.DrawText(e.Graphics,
+                  (e.RowIndex + 1).ToString(),
+                   dgv.RowHeadersDefaultCellStyle.Font,
+                   rectangle,
+                   dgv.RowHeadersDefaultCellStyle.ForeColor,
+                   TextFormatFlags.VerticalCenter | TextFormatFlags.Right);
+        }
+    }
+    }
